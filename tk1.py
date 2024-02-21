a9cc67426df81b5090d00aba1ec8d78f854ed34a
unknown <johnson21221@hotmail.com>
2024-02-21 04:26:03 -0700

    fix some issues, user can not enter what substring to replace

--- tk1.py
+++ tk1.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter import messagebox, filedialog
+from tkinter import BooleanVar, PhotoImage, StringVar, messagebox, filedialog
 
 root = tk.Tk()
 root.geometry('200x400')
@@ -8,14 +8,10 @@ root.minsize(400,400)
 root.maxsize(400,800)
 root.title('File rename')
 
-global substring
 global file_list
-global rename_count
 global dir
 
-substring = 'y2mate.com - '
 file_list = []
-rename_count = 0
 dir = './'
 
 # lists all files in the selected directory
@@ -25,10 +21,8 @@ def check_dir():
     file_list = os.listdir(dir)
 
     for file in file_list:
-        if substring in file:
+        if str_to_replace.get() in file:
             eligible_files += 1
-
-    export_list_as_txt()
 
     print(f'list of files in directory: {file_list}\n')
     lbl2.config(text = f'{eligible_files} file(s) eligible for renaming')
@@ -37,6 +31,11 @@ def export_list_as_txt():
     txt_name = './files_list.txt'
     count = 0
 
+    if not file_list:
+        print('No files')
+        messagebox.showinfo('No files', 'No files in directory')
+        return
+    
     while True:
         if os.path.exists(txt_name):
             print(f'{txt_name} already exists, changing name')
@@ -47,27 +46,25 @@ def export_list_as_txt():
 
     f = open(txt_name, 'a', encoding='utf-8')
 
-    if not file_list:
-        print('No files')
-        return
-    
     for item in file_list:
         f.write(item + '\n')
 
-# renames files in directory
 def rename_files():
     label_msg = ""
     rename_count = 0
 
     for file_name in file_list:
-        if substring in file_name:
-            new_name = os.path.join(dir, file_name[13:])
+        if str_to_replace.get() in file_name:
+            new_name = os.path.join(dir, file_name.replace(str_to_replace.get(), ''))
             old_name = os.path.join(dir, file_name)
 
             if not os.path.exists(new_name):
                 os.rename(old_name, new_name)
+            elif os.path.exists(new_name) and delete_dups.get():
+                os.remove(new_name)
+                os.rename(old_name, new_name)
             else:
-                print(f"File '{new_name}' already exists. Skipping renaming.")
+                print(f"File '{new_name}' already exists. Skipping file.")
                 continue
 
             print(f'Renamed {old_name} to {new_name}')
@@ -81,41 +78,93 @@ def rename_files():
 # changes directory where renaming happens
 def change_directory():
     global dir
-    new_dir = filedialog.askdirectory(initialdir= '/',
+    new_dir = filedialog.askdirectory(initialdir= './',
                                       title='Select directory')
-    
+
     if new_dir:
         dir = new_dir
         lbl1.config(text=f'Current directory: {dir}')
 
+def update_replace_lbl(var, index, mode):
+    print(str_to_replace.get())
+    replaceInfoLbl.config(text=f'Replacing files that contain: \'{str_to_replace.get()}\'')
+    
+###
+### Widgets
+###
+gen_width = 300
 
-###
-### Widgets 
-###
-lbl1 = tk.Label(root, 
-                text=f'Current directory: {dir}')
-lbl1.pack()
+window_width = root.winfo_width()
+print('window width ' + str(window_width))
+
+lbl1 = tk.Label(root,
+                text=f'Current directory: {dir}',
+                pady=10,
+                wraplength=gen_width,
+                justify='left')
+
+str_to_replace = StringVar()
+str_to_replace.set('y2mate.com - ')
+entry1 = tk.Entry(root,
+                  textvariable=str_to_replace,
+                  width=gen_width,
+                  justify='center')
 
 dirSelectBtn = tk.Button(root,
                          text = 'Change directory',
                          command = change_directory)
-dirSelectBtn.pack()
 
-checkBtn = tk.Button(root, 
-                     text = 'Check directory', 
+checkBtn = tk.Button(root,
+                     text = 'Check directory',
                      command = check_dir)
-checkBtn.pack()
 
-btn1 = tk.Button(root, 
-                 text = 'Rename file(s)', 
+downloadBtn = tk.Button(root,
+                        text='Download txt of files in directory',
+                        command=export_list_as_txt)
+
+btn1 = tk.Button(root,
+                 text = 'Rename file(s)',
                  activeforeground = "yellow",
                  command = rename_files)
-btn1.pack()
 
 lbl2 = tk.Label(root,
                 text = '',
-                wraplength=400)
-lbl2.pack()
+                wraplength=gen_width)
+
+replaceInfoLbl = tk.Label(root,
+                          text=f'Replacing files that contain: \'{str_to_replace.get()}\'')
+
+# optional settings
+ignore_cap = BooleanVar()
+delete_dups = BooleanVar()
+ignore_img = PhotoImage(file='./caps-lock.png')
+
+ignoreCapsCheck = tk.Checkbutton(root,
+                                text='Ignore capitalization',
+                                command='',
+                                variable=ignore_cap,
+                                onvalue=True,
+                                offvalue=False)
+
+deleteDupsCheck = tk.Checkbutton(root,
+                                 text='Delete duplicates when renamed',
+                                 variable=delete_dups,
+                                 onvalue=True,
+                                 offvalue=False)
+
+# packing order
+lbl1.pack(padx=5)
+entry1.pack(padx=5)
+replaceInfoLbl.pack(padx=5)
+ignoreCapsCheck.pack(padx=5)
+deleteDupsCheck.pack(padx=5)
+dirSelectBtn.pack(pady=5)
+checkBtn.pack(pady=5)
+downloadBtn.pack(pady=5)
+btn1.pack(pady=5)
+lbl2.pack(padx=5)
+
+str_to_replace.trace_add("write", update_replace_lbl)
 
 
 root.mainloop()
